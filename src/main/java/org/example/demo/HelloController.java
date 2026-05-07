aa4eb445826c8bd8ba3d42c1b1fc83dc31c1b02f
Naarai Bernal <110580725+raibernal03@users.noreply.github.com>
2026-05-07 11:13:50 -0500

    init

---
+++ src/main/java/org/example/demo/HelloController.java
@@ -0,0 +1,16 @@
+package org.example.demo;
+
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "DevOps Pipeline is Working!";
+    }
+
+}
